14c3ce21c223c3dbd7a0a3d9931efe1ca0ce7e64
nakanoyuki <yukich41@gmail.com>
2023-03-12 23:32:42 +0900

    背景に雪を降らせる

---
+++ snow-fall/script.js
@@ -0,0 +1,21 @@
+let snowContainer = document.querySelector(".snow-container")
+
+const createSnow =()=>{
+  let snow = document.createElement("span");
+  snow.className = "snow"
+
+  minSize=5;
+  maxSize=10;
+
+  let snowSize = Math.random() * 10;
+
+  snow.style.width = snowSize + "px"
+  snow.style.height = snowSize + "px"
+  snow.style.left =  Math.random() * 100 + "%";
+  snowContainer.appendChild(snow)
+  setTimeout(()=>{
+    snow.remove();
+  },10000)
+}
+
+setInterval(createSnow,100)
